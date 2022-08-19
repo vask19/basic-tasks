8b84428dc5ba2062f95994eb5f1abdf9d90ae05d
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-08-19 21:58:52 +0200

    streams

---
+++ src/loop/tasks10_20/Ex18.java
@@ -0,0 +1,19 @@
+package loop.tasks10_20;
+
+public class Ex18 {
+    public static void main(String[] args) {
+        int num = 22213233;
+
+        var c = (int)Math.pow(10,(num + "").length()-1);
+        int last = num%10;
+        int first =(num/c);
+        int newNum = num - (first * c) - last + first + (last * c);
+
+        System.out.println(newNum);
+
+
+
+
+
+    }
+}

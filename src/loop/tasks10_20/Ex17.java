02247c14c2c97bd52695abc469431d014635d905
Vasyl Korol mac <gusak.guska223@gmail.com>
2022-08-19 21:40:54 +0200

    streams

--- src/loop/tasks10_20/Ex17.java
+++ src/loop/tasks10_20/Ex17.java
@@ -1,34 +1,29 @@
 package loop.tasks10_20;
 
+import java.util.Arrays;
+
 public class Ex17 {
 
+
     public static void main(String[] args) {
-
-        int num = 256;
+        int num = 876432;
         int length = (num + "").length();
+        int sum;
+        int newNumber;
+        int count = 0;
+        int[] arr = new int[length];
 
-
-        System.out.println(num/100);
-        System.out.println(56 /10);
-
-      //  for (while)
-
-
-
-//        for (int i = 0;i<length;i++){
-//            x *=10;
-//            int temp = num - (num/x) * 10;
-//            num = (num - temp);
-//            System.out.println(x);
-//            System.out.println(num);
-//        }
-//        }
-        //   System.out.println(i + " " + j);
-
-        // int temp = num/10 *
-
-        //  System.out.println(temp);
-
+        for (int i =0;i<length-1;i++){
+            int div = 1;
+            for (int j=i;j<length-1;j++)
+                div *= 10;
+            newNumber = num/div;
+            num = num - (newNumber * div);
+            arr[count++] = newNumber;
+        }
+        arr[count++] = num;
+        sum = Arrays.stream(arr).reduce((ac,el) -> ac*el).getAsInt();
+        System.out.println(sum);
 
     }}
 

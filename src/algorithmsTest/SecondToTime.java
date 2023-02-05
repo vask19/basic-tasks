bb2e547e57b06a89a5a6f60ef0fda5b10719f761
Vasyl Korol mac <gusak.guska223@gmail.com>
2023-02-05 19:28:38 +0100

    seconds to time algoritm imlementation

---
+++ src/algorithmsTest/SecondToTime.java
@@ -0,0 +1,22 @@
+package algorithmsTest;
+
+import java.time.LocalTime;
+
+public class SecondToTime {
+    public static void main(String[] args) {
+        int ml = 77682;
+
+        int min = ml / 60;
+        int sec = ml - (min * 60);
+        int hour = min / 60;
+        min = min - (hour * 60);
+
+        LocalTime time = LocalTime.of(hour,min,sec);
+        System.out.println(time);
+
+
+
+
+    }
+
+}
